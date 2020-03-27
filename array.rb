7e8a3ac3fd29ba857744fd71848b084bbe9481be
Brandon Brown <brandonbrown4792@gmail.com>
2020-03-27 00:58:03 +0000

    Automatically backed up by Learn

---
+++ array.rb
@@ -0,0 +1,8 @@
+my_array = []
+puts "Array before: #{my_array}"
+my_array << "Brandon"
+puts "Array after: #{my_array}"
+
+picnic_ingredients = ["wine", "jalapeños", "donkey feed"]
+picnic_ingredients[2] = "Belgian chocolate"
+puts "Picnic Ingredients: #{picnic_ingredients}"
